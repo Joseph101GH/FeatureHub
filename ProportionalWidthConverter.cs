5674c837ce553065dd9886649596651063224013
Joseph <joseph1@live.be>
2023-05-08 22:16:10 +0200

    Working feature 3.

---
+++ ProportionalWidthConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace FeatureHub
+{
+    public class ProportionalWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double listViewWidth = (double)values[0];
+            double factor = System.Convert.ToDouble(parameter);
+            double totalFactors = values.Skip(1).Sum(x => System.Convert.ToDouble(x));
+            return (listViewWidth - 100) * factor / totalFactors; // 100 is the fixed width for the "Stop" column
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
