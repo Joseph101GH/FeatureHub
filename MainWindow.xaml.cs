6a8a9c73337755a3c87e137af96682a49ae1487f
Joseph <joseph1@live.be>
2023-05-22 21:47:03 +0200

    Titlebar removed and colors added. No rounded corners yet.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace FeatureHub
 {
@@ -9,6 +10,28 @@ namespace FeatureHub
         {
             InitializeComponent();
         }
+
+
+            private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                    this.DragMove();
+            }
+
+            private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+
+            private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+            {
+                this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            }
+
+            private void CloseButton_Click(object sender, RoutedEventArgs e)
+            {
+                this.Close();
+            }
 
         private void Feature1Button_Click(object sender, RoutedEventArgs e)
         {
